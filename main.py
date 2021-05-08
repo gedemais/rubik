14767fa8a75f273b66f82656017f51bacb291093
Geoffrey Demaison <geoffrey.demaiso@gmail.com>
2021-05-08 18:11:20 +0200

    ndarray tricks

--- main.py
+++ main.py
@@ -2,15 +2,25 @@ import numpy as np
 
 class   Cube():
     def __init__(self):
-        sides_names = ["Front", "Right", "Up", "Back", "Left", "Down"]
+        sides_names = ["F", "R", "U", "B", "L", "D"]
         sides_colors = ["B", "R", "Y", "G", "O", "W"]
         self.sides = {}
         for i, name in enumerate(sides_names):
             c = sides_colors[i]
-            self.sides[name] = np.ndarray((3, 3), dtype=list)
-            self.sides[name].fill(c)
+            self.sides[name] = np.random.rand(3, 3)
+            #self.sides[name] = np.ndarray((3, 3), dtype=list)
+            #self.sides[name].fill(c)
             print(name)
             print(self.sides[name])
+            print("----------------------------")
+            #self.sides[name] = np.moveaxis(self.sides[name], -1, 0)
+            #self.sides[name] = np.flip(self.sides[name], 0)
+
+            self.sides[name] = np.moveaxis(self.sides[name], -1, 0)
+            self.sides[name] = np.flip(self.sides[name], 1)
+            print(name)
+            print(self.sides[name])
+            print("----------------------------")
 
 
 def main():

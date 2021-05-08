d9b213ee85e9ed06fa9c9e45beb8cacda1dc4398
Geoffrey Demaison <geoffrey.demaiso@gmail.com>
2021-05-08 20:29:12 +0200

    instructions on the way

--- main.py
+++ main.py
@@ -7,24 +7,27 @@ class   Cube():
         self.sides = {}
         for i, name in enumerate(sides_names):
             c = sides_colors[i]
-            self.sides[name] = np.random.rand(3, 3)
-            #self.sides[name] = np.ndarray((3, 3), dtype=list)
-            #self.sides[name].fill(c)
-            print(name)
-            print(self.sides[name])
-            print("----------------------------")
-            #self.sides[name] = np.moveaxis(self.sides[name], -1, 0)
-            #self.sides[name] = np.flip(self.sides[name], 0)
+            self.sides[name] = (np.ndarray((3, 3), dtype=list), [])
+            self.sides[name].fill(c)
 
-            self.sides[name] = np.moveaxis(self.sides[name], -1, 0)
-            self.sides[name] = np.flip(self.sides[name], 1)
-            print(name)
-            print(self.sides[name])
-            print("----------------------------")
+        self.sides["F"][1] = [("U", "D"), ("L", "R"), ("R", "L"), ("D", "U")]
+        self.sides["R"][1] = [("U", "R"), ("F", "R"), ("B", "R"), ("D", "R")]
+        self.sides["U"][1] = [("L", "U"), ("R", "U"), ("F", "U"), ("B", "U")]
+        self.sides["B"][1] = [("", ""), ("", ""), ("", ""), ("", "")]
+        self.sides["L"][1] = [("", ""), ("", ""), ("", ""), ("", "")]
+        self.sides["D"][1] = [("", ""), ("", ""), ("", ""), ("", "")]
+
+    def move(self, side):
+        self.sides[side] = np.moveaxis(self.sides[side], -1, 0)
+        self.sides[side] = np.flip(self.sides[side], 1)
+    def prim_move(self, side):
+        self.sides[side] = np.moveaxis(self.sides[side], -1, 0)
+        self.sides[side] = np.flip(self.sides[side], 0)
 
 
 def main():
     cube = Cube()
+    print(cube.sides)
 
 
 if __name__ == "__main__":
